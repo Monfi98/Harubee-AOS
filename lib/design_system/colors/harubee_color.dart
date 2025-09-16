6f6e5456dcda746a676b65b5b36f45c6e810329b
shinseungjae <mok05247@naver.com>
2025-09-16 14:13:29 +0900

    Today View 요소 배치

---
+++ lib/design_system/colors/harubee_color.dart
@@ -0,0 +1,348 @@
+import 'dart:ui';
+
+enum Appearance { light, dark }
+
+class HarubeeColor {
+  static Color mainPrimary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0xFF5849DF);
+    }
+  }
+
+  static Color mainPrimary30(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x4D4956E4);
+      case Appearance.dark:
+        return const Color(0x4D5849DF);
+    }
+  }
+
+  static Color mainPrimary10(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x1A5849E4);
+      case Appearance.dark:
+        return const Color(0x265849DF);
+    }
+  }
+
+  static Color mainSecondary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF898EEB);
+      case Appearance.dark:
+        return const Color(0xFF5E50D7);
+    }
+  }
+
+  static Color mainTertiary60(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x99D6D6FD);
+      case Appearance.dark:
+        return const Color(0x335144C3);
+    }
+  }
+
+  static Color mainTertiary15(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x26D6D6FD);
+      case Appearance.dark:
+        return const Color(0x33877CE8);
+    }
+  }
+
+  static Color mainTertiary10(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x1AD6D6FD);
+      case Appearance.dark:
+        return const Color(0x1C877CE8);
+    }
+  }
+
+  static Color textPrimary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF21212E);
+      case Appearance.dark:
+        return const Color(0xFFFCFCFC);
+    }
+  }
+
+  static Color textPrimary30(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x4D21212E);
+      case Appearance.dark:
+        return const Color(0x59FCFCFC);
+    }
+  }
+
+  static Color textPrimary10(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x1A21212E);
+      case Appearance.dark:
+        return const Color(0x1AFCFCFC);
+    }
+  }
+
+  static Color textPrimary5(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x0D21212E);
+      case Appearance.dark:
+        return const Color(0x0DFCFCFC);
+    }
+  }
+
+  static Color textSecondary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF9AA5D1);
+      case Appearance.dark:
+        return const Color(0xFF5D608F);
+    }
+  }
+
+  static Color textSecondary40(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x669AA5D1);
+      case Appearance.dark:
+        return const Color(0x66393B66);
+    }
+  }
+
+  static Color textTertiary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFC4C2E0);
+      case Appearance.dark:
+        return const Color(0xFF64666F);
+    }
+  }
+
+  static Color textTertiary30(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x4DD6DAEC);
+      case Appearance.dark:
+        return const Color(0x4D64666F);
+    }
+  }
+
+  static Color textFixed = const Color(0xFFFEFEFE);
+  static Color bgPrimary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFFEFEFE);
+      case Appearance.dark:
+        return const Color(0xFF17172A);
+    }
+  }
+
+  static Color bgSecondary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFF0F0FE);
+      case Appearance.dark:
+        return const Color(0xFF45428A);
+    }
+  }
+
+  static Color bgSecondary50(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x80F0F0FE);
+      case Appearance.dark:
+        return const Color(0x4D413E85);
+    }
+  }
+
+  static Color bgAccent(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0x33413E85);
+    }
+  }
+
+  static Color warning(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFFF5252);
+      case Appearance.dark:
+        return const Color(0xFFD65252);
+    }
+  }
+
+  static Color warning10(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x1AFF5C5C);
+      case Appearance.dark:
+        return const Color(0x1AD65252);
+    }
+  }
+
+  static Color info(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF21212E);
+      case Appearance.dark:
+        return const Color(0xFF17172A);
+    }
+  }
+
+  static Color streak(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x4D5849E4);
+      case Appearance.dark:
+        return const Color(0xFF5F5CB3);
+    }
+  }
+
+  static Color alertBg(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x1A5849E4);
+      case Appearance.dark:
+        return const Color(0xFF28265C);
+    }
+  }
+
+  static Color alertText(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0xFFA4A1EF);
+    }
+  }
+
+  static Color hivePrimary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFFEFEFE);
+      case Appearance.dark:
+        return const Color(0xFF5849DF);
+    }
+  }
+
+  static Color hiveText(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0xFFFCFCFC);
+    }
+  }
+
+  static Color dayText(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0xFF17172A);
+    }
+  }
+
+  static Color mainText(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0xFF6F6FFF);
+    }
+  }
+
+  static Color hiveSecondary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF898EEB);
+      case Appearance.dark:
+        return const Color(0xFF3A1F9F);
+    }
+  }
+
+  static Color hiveSecondaryBack(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x4D21212E);
+      case Appearance.dark:
+        return const Color(0x4D4C3FBF);
+    }
+  }
+
+  static Color hiveSecondaryButton(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x99D6D6FD);
+      case Appearance.dark:
+        return const Color(0x665144C3);
+    }
+  }
+
+  static Color hiveSecondaryText(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x80F0F0FE);
+      case Appearance.dark:
+        return const Color(0x99877CE8);
+    }
+  }
+
+  static Color hivePrimaryBack(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFD6DAEC);
+      case Appearance.dark:
+        return const Color(0x4D7769FF);
+    }
+  }
+
+  static Color hivePrimaryButton(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x99D6D6FD);
+      case Appearance.dark:
+        return const Color(0x1AFCFCFC);
+    }
+  }
+
+  static Color mainBgPrimary(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFFFEFEFE);
+      case Appearance.dark:
+        return const Color(0x4D413E85);
+    }
+  }
+
+  static Color mainBgAccent(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0xFF5849E4);
+      case Appearance.dark:
+        return const Color(0xFF17172A);
+    }
+  }
+
+  static Color textSecondaryInversion(Appearance mode) {
+    switch (mode) {
+      case Appearance.light:
+        return const Color(0x80F0F0FE);
+      case Appearance.dark:
+        return const Color(0x4DFEFEFE);
+    }
+  }
+}
--- lib/presentation/today/today_view.dart
+++ lib/presentation/today/today_view.dart
@@ -1,40 +1,178 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:harubee/design_system/colors/harubee_colors.dart';
+import 'package:harubee/design_system/colors/harubee_color.dart';
 
+// * TodayView
 class TodayView extends StatelessWidget {
   const TodayView({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    // TODO: Provider 도입
     final mode = Theme.of(context).brightness == Brightness.dark
         ? Appearance.dark
         : Appearance.light;
 
     return Scaffold(
-      backgroundColor: HarubeeColors.mainPrimary(mode),
+      backgroundColor: HarubeeColor.mainPrimary(mode),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         actions: [
           IconButton(
             icon: const Icon(CupertinoIcons.question_circle),
-            color: HarubeeColors.textFixed,
+            color: HarubeeColor.textFixed,
             onPressed: () {
               debugPrint("Search tapped");
             },
           ),
           IconButton(
             icon: const Icon(CupertinoIcons.gear),
-            color: HarubeeColors.textFixed,
+            color: HarubeeColor.textFixed,
             onPressed: () {
               debugPrint("Settings tapped");
             },
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Today View Screen'),
+      body: const Stack(children: [BackgroundLayer(), ForegroundLayer()]),
+    );
+  }
+}
+
+// * BackgroundLayer
+class BackgroundLayer extends StatelessWidget {
+  const BackgroundLayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: double.infinity, child: Text("BackgroundLayer"));
+  }
+}
+
+// * ForegroundLayer
+class ForegroundLayer extends StatelessWidget {
+  const ForegroundLayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: Text(
+            "2024년 9월 24일 (수)",
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: HarubeeColor.textFixed,
+            ),
+          ),
+        ),
+        FooterSection(),
+      ],
+    );
+  }
+}
+
+// * FooterSection
+class FooterSection extends StatelessWidget {
+  const FooterSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Provider 도입
+    final mode = Theme.of(context).brightness == Brightness.dark
+        ? Appearance.dark
+        : Appearance.light;
+
+    return Container(
+      height: 230,
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 14, bottom: 9),
+      color: HarubeeColor.mainBgPrimary(mode),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "캘린더",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: HarubeeColor.textPrimary(mode),
+                ),
+              ),
+              Icon(CupertinoIcons.chevron_forward, size: 16),
+            ],
+          ),
+          SizedBox(height: 8),
+          StreakCalender(),
+          SizedBox(height: 16),
+          TextButton(
+            style: TextButton.styleFrom(
+              minimumSize: Size.fromHeight(60),
+              backgroundColor: HarubeeColor.mainPrimary(mode),
+              foregroundColor: HarubeeColor.textFixed,
+              textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+            onPressed: () {
+              debugPrint("Bottom Button 눌림");
+            },
+            child: Text("실제 지출 및 수입 입력하기"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// * StreakCalendar
+class StreakCalender extends StatelessWidget {
+  const StreakCalender({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final mode = Theme.of(context).brightness == Brightness.dark
+        ? Appearance.dark
+        : Appearance.light;
+
+    final flexValues = [3, 1, 3];
+
+    return InkWell(
+      onTap: () {
+        debugPrint("Streak Tapped!");
+      },
+      child: SizedBox(
+        height: 72,
+        child: Row(
+          children: List.generate(flexValues.length * 2 - 1, (index) {
+            if (index.isOdd) {
+              return const SizedBox(width: 8);
+            } else {
+              return Expanded(
+                flex: flexValues[index ~/ 2],
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: HarubeeColor.bgSecondary50(mode),
+                    border: Border.all(
+                      color: index == 2
+                          ? HarubeeColor.mainSecondary(mode)
+                          : Colors.transparent,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              );
+            }
+          }),
+        ),
       ),
     );
   }
